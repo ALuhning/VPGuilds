42a9383b36224f7be53c0e84364f772c484834ba
Aaron Luhning <a.luhning@vitalpoint.ai>
2020-08-04 07:06:54 -0400

    fleek deploy

--- src/config.js
+++ src/config.js
@@ -4,15 +4,15 @@ const CONTRACT_NAME = process.env.CONTRACT_NAME;
 function getConfig(env) {
     switch (env) {
 
-    case 'production':
-    case 'mainnet':
-        return {
-            networkId: 'mainnet',
-            nodeUrl: 'https://rpc.mainnet.near.org',
-            contractName: CONTRACT_NAME,
-            walletUrl: 'https://wallet.mainnet.near.org',
-            helperUrl: 'https://helper.mainnet.near.org',
-        };
+  //  case 'production':
+  //  case 'mainnet':
+  //      return {
+  //          networkId: 'mainnet',
+  //          nodeUrl: 'https://rpc.mainnet.near.org',
+  //          contractName: CONTRACT_NAME,
+   //         walletUrl: 'https://wallet.mainnet.near.org',
+   //         helperUrl: 'https://helper.mainnet.near.org',
+   //     };
     case 'development':
     case 'testnet':
         return {
